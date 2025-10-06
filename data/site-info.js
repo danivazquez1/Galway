0e774ce1d4ca364474de82d109bc34d1299f5124
Daniel Vázquez Otero <104627433+danivazquez1@users.noreply.github.com>
2025-10-06 20:33:54 +0200

    Update Galway menu data

---
+++ data/site-info.js
@@ -0,0 +1,9 @@
+window.siteInfo = {
+  direccion: "Cra. 45 # 53-50",
+  barrio: "Barrio Manila, Medellín",
+  mapa: "https://maps.google.com/?q=Galway+Bar",
+  telefono: "+57 604 1234567",
+  whatsapp: "+573001234567",
+  correo: "reservas@galwaybar.com",
+  instagram: "https://www.instagram.com/galwaybar",
+};
